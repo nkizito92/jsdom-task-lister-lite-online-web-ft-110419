54513ebf2da26450c195c7adde87cc38bea52fc2
Kizito Njoku <nkizito57@gmail.com>
2020-03-04 22:08:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let display = document.querySelector("#tasks");
   let submit = document.querySelector("#submitted");
+<<<<<<< HEAD
   let textField = document.querySelector("#new-task-description");
   submit.addEventListener("click", (e) => {
     if (textField.value === "") {
@@ -11,6 +12,16 @@ document.addEventListener("DOMContentLoaded", () => {
       display.innerHTML += `<li class="bull">${textField.value}</li>`;
       e.preventDefault();
     }
+=======
+  let textField = document.querySelector("#text");
+    submit.addEventListener("click", (e) => { 
+        if (textField === null) {
+         display.innerHTML += `<li>${textField.value}</li>`;
+          
+        } else {
+          
+        }
+>>>>>>> 5c6fde00c2bdb38bd0de28c1bc22152a1bc8f29f
 
   });
 });
