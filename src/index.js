80df532dfadb996122e32bc97fac63e816cf61f1
nkizito92 <nkizito57@gmail.com>
2020-03-04 13:46:02 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let display = document.querySelector("#tasks");
   let submit = document.querySelector("#submitted");
-  let textField = document.querySelector("#text");
-    submit.addEventListener("click", (e) => { 
-        if (textField === null) {
-         display.innerHTML += `<li>${textField.value}</li>`;
-          
-        } else {
-          
-        }
+  let textField = document.querySelector("#new-task-description");
+  submit.addEventListener("click", (e) => {
+    if (textField.value === "") {
+      alert("Please Fill out the text Field!");
+      e.preventDefault();
+    } else {
+      display.innerHTML += `<li class="bull">${textField.value}</li>`;
+      e.preventDefault();
+    }
 
   });
 });
