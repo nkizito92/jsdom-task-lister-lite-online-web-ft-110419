732a2af021f17f883389e782158d6214b3725a13
Kizito Njoku <nkizito57@gmail.com>
2020-03-04 19:23:16 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let display = document.querySelector("#tasks");
+  let submit = document.querySelector("#submitted");
+  let textField = document.querySelector("#text");
+    submit.addEventListener("click", (e) => { 
+        if (textField === null) {
+         display.innerHTML += `<li>${textField.value}</li>`;
+          
+        } else {
+          
+        }
+
+  });
 });
